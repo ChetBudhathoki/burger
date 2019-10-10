de0c03132b4d269d536f6351f97254056fe6c40b
Chet Budhathoki <okchet@gmail.com>
2019-10-10 11:25:15 -0600

    Updated all the files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,8 @@
 -- Select burgers_db
-USE `burgers_db`;
+USE burgers_db;
 
 -- insert 3 burgers
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Angus', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Salmon', false, CURRENT_TIMESTAMP);
-INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Mushroom', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Salmon', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('BLT', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Angus', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Mushroom', false, CURRENT_TIMESTAMP);
